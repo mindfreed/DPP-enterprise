471baf0da558e88072ec867296f62e4b5e2f052c
mindfreed <kris.freed@freedmindai.com>
2025-12-30 11:05:38 -0800

    Update main.py
    Deploy fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from dpp_final import app  # Your DPP code
+from dpp_final import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080)
